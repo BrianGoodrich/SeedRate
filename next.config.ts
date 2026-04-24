0a481f301d99798d3c413310cadb488dd0c710b7
BrianGoodrich <bgoodrich201@gmail.com>
2026-04-24 00:01:50 -0600

    adding asset prefix

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: '/SeedRate', 
+  assetPrefix: '/SeedRate/',
 };
 
 export default nextConfig;
