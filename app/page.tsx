d9f9a4e41b350ee2b7a74b1654d81aae45ae68c5
BrianGoodrich <bgoodrich201@gmail.com>
2026-04-23 23:56:03 -0600

    image fix

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-8 px-4 bg-white dark:bg-black sm:items-center">
         <Image
-          src="/seedratelogo.png"
+          src="SeedRate/seedratelogo.png"
           alt="seedrate logo"
           width={500}
           height={100}
