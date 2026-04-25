9dfe333090ffc1642bee35db019a5a0bf2ac864e
BrianGoodrich <bgoodrich201@gmail.com>
2026-04-25 00:21:06 -0600

    asasasass

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: 'export', // Required for GitHub Pages
+  basePath: '/your-repository-name', 
+  assetPrefix: '/your-repository-name/', 
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
