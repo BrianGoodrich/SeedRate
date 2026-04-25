c4d3dbae986004aa779f026173dd1610dc15a345
BrianGoodrich <bgoodrich201@gmail.com>
2026-04-25 00:39:45 -0600

    c

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: 'export', // Required for GitHub Pages
-  basePath: '/your-repository-name', 
-  assetPrefix: '/your-repository-name/', 
-  images: { unoptimized: true },
 };
 
 export default nextConfig;
