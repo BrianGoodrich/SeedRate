347b03929c88241ba1afddb118c7dc6bca4f32ef
BrianGoodrich <bgoodrich201@gmail.com>
2026-04-25 01:02:03 -0600

    vvv

--- app/page.tsx
+++ app/page.tsx
@@ -20,8 +20,6 @@ export default function Home() {
   const [showFlash, setShowFlash] = useState(false);
   const prefix = process.env.NODE_ENV === "production" ? "/SeedRate" : "";
 
-  alert("test");
-
   const calibrate = () => {
     //Get area covered
     const area =
